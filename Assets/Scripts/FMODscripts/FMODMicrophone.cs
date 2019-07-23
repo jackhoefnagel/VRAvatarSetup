52aa370b4954b696d7a0a9df66a426e93188769e
Jack Hoefnagel <jackhoefnagel@gmail.com>
2019-07-23 13:30:28 +0200

    fixing .gitignore

---
+++ Assets/Scripts/FMODscripts/FMODMicrophone.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using FMOD;
+
+public class FMODMicrophone : MonoBehaviour
+{
+    private FMOD.Studio.System sys;
+
+    private void Start()
+    {
+        sys.initialize(4, FMOD.Studio.INITFLAGS.NORMAL, INITFLAGS.NORMAL, new System.IntPtr(0));
+        //sys.get
+    }
+}
